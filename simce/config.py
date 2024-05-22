94e73e19b54155341124752b3776bca9b69a4b58
Juan Concha <juanelo574@gmail.com>
2024-05-22 14:29:01 -0400

    fix config

--- simce/config.py
+++ simce/config.py
@@ -9,12 +9,12 @@ from pathlib import Path
 
 dir_data = Path('data/')
 dir_input = dir_data / 'input'
-dir_estudiantes = dir_input / 'CE22'
+dir_estudiantes = dir_input / 'CE'
 dir_padres = dir_input / 'CP'
 
-dir_output = Path('data/output/output_subpreg22')
+dir_output = Path('data/output/output_subpreg')
 dir_tabla_99 = Path('data/output/output_tabla_99/')
-dir_insumos = Path('data/output/insumos22/')
+dir_insumos = Path('data/output/insumos/')
 
 regex_estudiante = r'(\d{7,})_.*jpg'
 
