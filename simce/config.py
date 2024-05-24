c63fb5536005074dda8c5af8665a747bfc2b997b
Juan Concha <juanelo574@gmail.com>
2024-05-24 14:21:35 -0400

    actualización configuraciones

--- simce/config.py
+++ simce/config.py
@@ -16,8 +16,19 @@ dir_output = Path('data/output/output_subpreg')
 dir_tabla_99 = Path('data/output/output_tabla_99/')
 dir_insumos = Path('data/output/insumos/')
 
+
+# PROCESAMIENTO POSIBLES DOBLES MARCAS
+nombre_tabla_estud_origen = 'CE_Origen_DobleMarca.csv'
+nombre_tabla_estud_final = 'CE_Final_DobleMarca.csv'
+nombre_tabla_padres_origen = 'CP_Origen_DobleMarca.csv'
+nombre_tabla_padres_final = 'CP_Final_DobleMarca.csv'
+variables_identificadorras = ['rbd', 'dvRbd', 'codigoCurso', 'serie', 'rutaImagen1']
+
+
 regex_estudiante = r'(\d{7,})_.*jpg'
 
 # IMPORTANTE: parámetro que define cuántos píxeles espera como
 # mínimo entre línea y línea de cada subpregunta
 n_pixeles_entre_lineas = 22
+n_preg_ignoradas_estudiantes = 2
+n_preg_ignoradas_padres = 1
