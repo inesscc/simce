30f8b79ab16e584d09069b0927e8beceb6eff9c1
Juan Concha <juanelo574@gmail.com>
2024-07-03 17:45:46 -0400

    6 rondas

--- simce/preparar_modelamiento.py
+++ simce/preparar_modelamiento.py
@@ -184,6 +184,8 @@ def transform_img(path_img, i):
 
         trans_img = v2.GaussianBlur(kernel_size = (3, 5), sigma = (1, 2)) (orig_img)
        # trans_img = v2.ColorJitter(brightness=0.1, contrast=0.2, saturation=0.2, hue=0.01)(trans_img)
+    if i >= 5:
+        trans_img = v2.ColorJitter(brightness=0.08, contrast=0.15, saturation=0.15, hue=0.005)(trans_img)
     else:
 
         raise 'Función acepta hasta 5 rondas de data augmentation'
